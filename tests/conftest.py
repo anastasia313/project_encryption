9c97cbef5aa1830e2d91481575f3fb1ae24a859b
anastasia313 <nastasia.parfenovich@gmail.com>
2023-11-28 15:01:35 +0100

    update test_write_json

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,13 @@
 from src.cipher import Cipher, EnteredZeroStep, EnteredNegativeStep, EnteredStepMoreThanAlphabet
 import pytest
 
-@pytest.fixture
-def prepare_db(connection):
-    cursor = connection.cursor()
-    cursor.execute(
-        'CREATE TABLE IF NOT EXISTS Ciphers(id INTEGER PRIMARY KEY, date DATE NOT NULL, cipher TEXT NOT NULL)')
-    cursor.execute('DELETE FROM Ciphers')
-    date = "2023-11-20 15:11:22.906568"
-    cipher = "{'cipher':'bcde'}"
-    cursor.execute("INSERT INTO Ciphers(date, cipher) VALUES(?, ?)", (date, cipher))
-    connection.commit()
+# @pytest.fixture
+# def prepare_db(connection):
+#     cursor = connection.cursor()
+#     cursor.execute(
+#         'CREATE TABLE IF NOT EXISTS Ciphers(id INTEGER PRIMARY KEY, date DATE NOT NULL, cipher TEXT NOT NULL)')
+#     cursor.execute('DELETE FROM Ciphers')
+#     date = "2023-11-20 15:11:22.906568"
+#     cipher = "{'cipher':'bcde'}"
+#     cursor.execute("INSERT INTO Ciphers(date, cipher) VALUES(?, ?)", (date, cipher))
+#     connection.commit()
--- tests/test_write_json_data.py
+++ tests/test_write_json_data.py
@@ -6,16 +6,23 @@ from src.const import JSON_RESULT_FILE_NAME
 import pytest
 
 
-def test_write_json_data(prepare_db):
+def test_write_json_data():
     db_path = "../tests/json_db.sqlite3"
     with open(JSON_RESULT_FILE_NAME, 'w'):
         pass
 
     with sqlite3.connect(db_path) as connection:
-        prepare_db(connection)
+        cursor = connection.cursor()
+        cursor.execute('CREATE TABLE IF NOT EXISTS Ciphers(id INTEGER PRIMARY KEY, date DATE NOT NULL, cipher TEXT NOT NULL)')
+        cursor.execute('DELETE FROM Ciphers')
+        date = "2023-11-20 15:11:22.906568"
+        cipher = "{'cipher':'bcde'}"
+        cursor.execute("INSERT INTO Ciphers(date, cipher) VALUES(?, ?)", (date, cipher))
+        connection.commit()
 
     Cipher.write_json_data(db_path)
 
     with open(JSON_RESULT_FILE_NAME, 'r', encoding='utf-8') as file:
         data = file.read()
     assert "[{\"1\": [1, \"2023-11-20 15:11:22.906568\", \"{'cipher':'bcde'}\"]}]" == data
+
